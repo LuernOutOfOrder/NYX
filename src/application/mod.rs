2a1d1de24b72b84a22e12b5d70e9f537d774b577
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-21 17:24:59 +0100

    fix: the error when changing work dir

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,7 +1,7 @@
 use inquire::{InquireError, Select};
 use std::env;
-use std::path::Path;
-use std::process::Command;
+use std::fs;
+use std::path::PathBuf;
 
 pub fn new_nodejs_app(name: String) {
     let options: Vec<&str> = vec!["Node.js", "Python", "Golang", "Rust", "C++"];
@@ -13,19 +13,13 @@ pub fn new_nodejs_app(name: String) {
         Ok(choice) => println!("{}!", choice),
         Err(_) => println!("There was an error, please try again"),
     }
-    match Command::new("mkdir").arg(name.clone()).spawn() {
-        Ok(_) => println!("Successfully created the new application directory"),
+    match fs::create_dir(name.clone()) {
+        Ok(_) => println!("Directory created successfully"),
         Err(e) => println!("Failed to create directory: {}", e),
     }
-    change_work_dir(name.clone());
-    // match Command::new("mkdir").arg(name).spawn() {
-    //     Ok(_) => println!("Directory created successfully"),
-    //     Err(e) => println!("Failed to create directory: {}", e),
-    // }
+    change_work_dir(&name);
 }
 
-fn change_work_dir(dir: String) {
-    let new_app_path = format!("/{}\n", dir);
-    print!("{}", new_app_path);
-    env::set_current_dir(&new_app_path).expect("Failed to change directory");
+fn change_work_dir(dir: &String) {
+    env::set_current_dir(&dir).expect("Failed to change directory");
 }
