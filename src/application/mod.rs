696d905a8b9a2d794b202ebba7d2559b887859e3
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-21 16:39:04 +0100

    feat: add change workdir function

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,4 +1,6 @@
 use inquire::{InquireError, Select};
+use std::env;
+use std::path::Path;
 use std::process::Command;
 
 pub fn new_nodejs_app(name: String) {
@@ -11,8 +13,19 @@ pub fn new_nodejs_app(name: String) {
         Ok(choice) => println!("{}!", choice),
         Err(_) => println!("There was an error, please try again"),
     }
-    match Command::new("mkdir").arg(name).spawn() {
-        Ok(_) => println!("Directory created successfully"),
+    match Command::new("mkdir").arg(name.clone()).spawn() {
+        Ok(_) => println!("Successfully created the new application directory"),
         Err(e) => println!("Failed to create directory: {}", e),
     }
+    change_work_dir(name.clone());
+    // match Command::new("mkdir").arg(name).spawn() {
+    //     Ok(_) => println!("Directory created successfully"),
+    //     Err(e) => println!("Failed to create directory: {}", e),
+    // }
+}
+
+fn change_work_dir(dir: String) {
+    let new_app_path = format!("/{}\n", dir);
+    print!("{}", new_app_path);
+    env::set_current_dir(&new_app_path).expect("Failed to change directory");
 }
