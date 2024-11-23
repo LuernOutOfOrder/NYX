24ac5a3d6d832cd0678d7125c8dcfc9c75521c21
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-23 14:59:44 +0100

    feat: add the generate nodejs app features

--- src/about.rs
+++ src/about.rs
@@ -1,3 +1,4 @@
+//TODO check to print in color
 pub fn about() {
     println!(
         r"         
--- src/application/mod.rs
+++ src/application/mod.rs
@@ -2,6 +2,7 @@ use inquire::{InquireError, Select};
 use std::env;
 use std::fs;
 use std::process::Command;
+mod templates;
 
 pub fn new_project(name: String) {
     let options: Vec<&str> = vec!["Node.js", "Python", "Golang", "Rust", "C++"];
@@ -28,21 +29,41 @@ fn new_app_by_choice(tech: &str) {
 }
 
 fn new_nodejs_app() {
-    Command::new("npm")
+    let mut npm = Command::new("npm")
         .arg("init")
         .arg("-y")
         .spawn()
         .expect("npm command failed to execute");
-    Command::new("npm")
+    let status = npm.wait().expect("Failed to wait on npm process");
+    if !status.success() {
+        panic!("Error init npm app");
+    }
+    let mut ts = Command::new("npm")
         .arg("install")
         .arg("--save-dev")
         .arg("typescript")
         .spawn()
         .expect("failed to install typescript");
+    let ts_status = ts
+        .wait()
+        .expect("Failed to wait on typescript install process");
+    if !ts_status.success() {
+        panic!("Error installing typescript");
+    }
     Command::new("touch")
         .arg("tsconfig.json")
         .spawn()
         .expect("failed to generate tsconfig.json");
+    let tech = "Node.js";
+    generate_gitignore(&tech);
+    println!("Successfully generate the new Node.js application")
+}
+
+fn generate_gitignore(tech: &str) -> String {
+    match tech {
+        tech if tech == "Node.js" => templates::nodejs_gitignore_template(),
+        _ => "please select a tech".to_string(),
+    }
 }
 
 fn change_work_dir(dir: &String) {
