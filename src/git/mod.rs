b67b1d5b3899343e4e4f0bad6804c9b3578e50eb
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-29 12:25:05 +0100

    feat: improve stash command

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,8 +1,19 @@
 use std::process::Command;
 
+use inquire::Text;
+
+use crate::utils;
+
 pub fn nyx_git_stash() {
+    inquire::set_global_render_config(utils::get_render_config());
+    let message = Text::new("Enter stash message")
+        .prompt()
+        .expect("Failed to read stash message");
     let mut stash = Command::new("git")
         .arg("stash")
+        .arg("push")
+        .arg("-m")
+        .arg(&message)
         .spawn()
         .expect("Failed to stash local change");
     let stash_status = stash.wait().expect("Failed to wait stash command");
