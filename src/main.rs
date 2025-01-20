1509c261b4fabe3b58c6eaeff3ac7bb97528f562
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-20 10:53:36 +0100

    feat: update version to 1.0 and reflect changes in main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::{env, process::exit};
 // Current version of NYX
 // if modified and then running update command it will replace
 // your current nyx installation with the newer version
-const VERSION: &'static str = "0.9.94";
+const VERSION: &'static str = "1.0";
 
 #[derive(Debug, Clone)]
 enum Commands {
