d155cd54613591d044224f179b3de5831b5179b7
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-13 14:05:01 +0100

    feat: add bincode serialization for NXS file creation and update command initialization

--- src/projects/binary.rs
+++ src/projects/binary.rs
@@ -7,8 +7,10 @@ It includes functionalities for reading, writing, and manipulating the binary da
 
 use crate::utils;
 
-use std::fs;
+use bincode;
+use serde::Serialize;
 use std::path::Path;
+use std::{fs, vec};
 
 pub struct NXSFile {
     pub header: Header,
@@ -16,25 +18,24 @@ pub struct NXSFile {
 }
 
 pub struct Header {
-    magic_number: u32,
-    project_count: u32,
-    project_list: u32,
-    format_version: u8,
+    magic_number: &'static [u8],
+    format_version: &'static [u8],
+    project_count: u8,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct ProjectList {
     pub entries: Vec<ProjectEntry>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct ProjectEntry {
     pub project_hash: String,
     pub project_id: String,
     pub project_size: u32,
 }
 
-pub fn create_nxs() {
+pub fn create_data() {
     utils::change_work_dir(&utils::get_nyx_env_var());
     if Path::new(".data").exists() {
         lrncore::logs::info_log("Reinitialized data folder");
@@ -50,4 +51,36 @@ pub fn create_nxs() {
             lrncore::logs::error_log(&format!("Failed to remove existing .data directory: {}", e));
         }
     }
+    create_nxs_file();
+}
+
+fn create_nxs_file() {
+    // header
+    let header: Header = Header {
+        magic_number: b"NXS\0",
+        format_version: b"0.1.0\0",
+        project_count: 0,
+    };
+    let mut header_buff: Vec<u8> = Vec::new();
+    header_buff.extend_from_slice(header.magic_number);
+    header_buff.extend_from_slice(header.format_version);
+    header_buff.push(header.project_count);
+    header_buff.extend_from_slice(b" ");
+    // project list
+    let project_list: ProjectList = ProjectList {
+        entries: Vec::new(),
+    };
+    let mut project_list_buff: Vec<u8> = Vec::new();
+    let project_list_bytes =
+        bincode::serialize(&project_list).expect("Failed to serialize project list");
+    project_list_buff.extend_from_slice(&project_list_bytes);
+    // complete file
+    let file: NXSFile = NXSFile {
+        header: header,
+        project_list: project_list,
+    };
+    let mut file_buff: Vec<u8> = Vec::new();
+    file_buff.extend_from_slice(&header_buff);
+    file_buff.extend_from_slice(&project_list_buff);
+    println!("{:?}", String::from_utf8_lossy(&file_buff));
 }
