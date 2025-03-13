a294c592fa94745152d1bf14186f62633169c9aa
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-13 17:53:22 +0100

    chore: update documentation, .gitignore and add empty NXP structure

--- src/projects/binary.rs
+++ src/projects/binary.rs
@@ -1,7 +1,7 @@
 /*!
-Module to handle the NXS (nyx storage) binary format to store projects data.
+Module to handle the NXS (nyx storage) and NXP (nyx project) binary format to store projects data.
 
-This module provides the definition of data structures and methods to create and handle the binary format used for storing project data in the NXS format.
+This module provides the definition of data structures and methods to create and handle the binary format used for storing project data in the NXS format and the NXP format.
 It includes functionalities for reading, writing, and manipulating the binary data to ensure efficient storage and retrieval.
 */
 
@@ -9,33 +9,37 @@ use crate::utils;
 
 use bincode;
 use serde::Serialize;
+use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use std::{fs, vec};
 
-pub struct NXSFile {
-    pub header: Header,
-    pub project_list: ProjectList,
-}
+/*
+NXS file structure
+*/
 
-pub struct Header {
+struct Header {
     magic_number: &'static [u8],
     format_version: &'static [u8],
     project_count: u8,
 }
 
 #[derive(Debug, Clone, Serialize)]
-pub struct ProjectList {
+struct ProjectList {
     pub entries: Vec<ProjectEntry>,
 }
 
 #[derive(Debug, Clone, Serialize)]
-pub struct ProjectEntry {
+struct ProjectEntry {
     pub project_hash: String,
     pub project_id: String,
     pub project_size: u32,
 }
+
+/*
+NXP file structure
+*/
+struct Project {}
 
 pub fn create_data() {
     utils::change_work_dir(&utils::get_nyx_env_var());
@@ -46,7 +50,7 @@ pub fn create_data() {
             lrncore::logs::error_log(&format!("Failed to remove existing .data directory: {}", e));
         }
     }
-    let data_folder = match std::fs::create_dir(".data") {
+    match std::fs::create_dir(".data") {
         Ok(_) => (),
         Err(e) => {
             lrncore::logs::error_log(&format!("Failed to remove existing .data directory: {}", e));
@@ -78,10 +82,6 @@ fn create_nxs_file() {
         bincode::serialize(&project_list).expect("Failed to serialize project list");
     project_list_buff.extend_from_slice(&project_list_bytes);
     // complete file
-    let file: NXSFile = NXSFile {
-        header: header,
-        project_list: project_list,
-    };
     let mut file_buff: Vec<u8> = Vec::new();
     file_buff.extend_from_slice(&header_buff);
     file_buff.extend_from_slice(&project_list_buff);
@@ -92,7 +92,7 @@ fn create_nxs_file() {
             return;
         }
     };
-    let mut nxs_file_write = match nxs_file.write_all(&file_buff) {
+    match nxs_file.write_all(&file_buff) {
         Ok(_) => (),
         Err(e) => {
             lrncore::logs::error_log(&format!("Failed to write buffer in nxs file: {}", e));
