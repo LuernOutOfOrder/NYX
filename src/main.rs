20cccfd51213ddb2e469c702624dfa5a146689a0
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-04-04 17:23:28 +0200

    chore: bump to 1.12.2

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use std::env;
 // Current version of NYX
 // if modified and then running update command it will replace
 // your current nyx installation with the newer version
-const VERSION: &str = "1.12.1";
+const VERSION: &str = "1.12.2";
 #[derive(Debug, Clone)]
 enum Commands {
     Init,
